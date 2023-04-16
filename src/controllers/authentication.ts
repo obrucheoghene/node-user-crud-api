ab71cdf0a5f2759f0d392bcff567e2e5f7243e73
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-16 23:49:39 +0100

    Create login route

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -2,6 +2,48 @@ import { createUser, getUserByEmail } from '../db/users';
 import express from 'express';
 import { authentication, random } from '../helpers';
 
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: 'Email and password are required' });
+    }
+
+    const user = await getUserByEmail(email).select(
+      '+authentication.salt +authentication.password'
+    );
+
+    if (!user) {
+      return res.status(400).json({ message: 'User does not exit' });
+    }
+
+    const expectedHash = authentication(user.authentication.salt, password);
+
+    if (user.authentication.password !== expectedHash) {
+      return res.status(403).json({ message: 'Wrong username or password' });
+    }
+
+    const salt = random();
+
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+
+    await user.save();
+
+    res.cookie('NODE_AUTH', user.authentication.sessionToken, {
+      domain: 'localhost',
+      path: '/',
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {}
+};
+
 export const register = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password, username } = req.body;
@@ -9,7 +51,7 @@ export const register = async (req: express.Request, res: express.Response) => {
     if (!email || !password || !username) {
       return res
         .status(400)
-        .json({ message: 'Email, password and username is required' });
+        .json({ message: 'Email, password and username are required' });
     }
 
     const existingUser = await getUserByEmail(email);
