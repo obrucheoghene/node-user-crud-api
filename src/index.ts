7e56b6d7f45262cd54698c1ea6380899cece96be
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-16 21:21:26 +0100

    Setup mongoose to  connect to mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log('Hello Node API');
+import express from 'express';
+import http from 'http';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(5000, () => {
+  console.log('Server is running on http://localhost:5000');
+});
+const MONGO_DB_URL = process.env.DB_URL;
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_DB_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
