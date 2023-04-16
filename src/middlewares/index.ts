4598ebc21e24c53b5ca93b807e42a3c592db604b
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-17 00:33:27 +0100

    Create isOwner middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -4,6 +4,31 @@ import { get, merge } from 'lodash';
 
 import { getUserBySessionToken } from '../db/users';
 
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+
+    const currentUserId = get(req, 'identity._id') as string;
+    console.log(currentUserId);
+    if (!currentUserId) {
+      return res.sendStatus(403);
+    }
+
+    if (currentUserId.toString() !== id) {
+      return res.sendStatus(403);
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
 export const isAuthenticated = async (
   req: express.Request,
   res: express.Response,
